14c203b4cc3640b5fb790316ba22bfab6060c152
Stephen Firrincieli <sfirrin@unc.edu>
2016-12-06 12:55:00 -0500

    Finish day 5 Python

---
+++ day05.py
@@ -0,0 +1,41 @@
+import hashlib
+
+def day05a(inp):
+    password = ''
+    pt2password = '        '
+    index = 0
+    while len(password) < 8:
+        md5 = hashlib.md5()
+        md5.update(inp + str(index))
+        hashed = md5.hexdigest()
+        if hashed[:5] == '00000':
+            password += hashed[5]
+            pt2index = int(hashed[5])
+            print(password)
+        index += 1
+    print('The final password is: ' + password)
+
+def day05b(inp):
+    password = [' ' for i in range(8)]
+    found_chars = 0
+    index = 0
+    while found_chars < 8:
+        md5 = hashlib.md5()
+        md5.update(inp + str(index))
+        hashed = md5.hexdigest()
+        if hashed[:5] == '00000':
+            passindex = int(hashed[5], 16)
+            if passindex < 8 and password[passindex] == ' ':
+                password[passindex] = hashed[6]
+                found_chars += 1
+                print(password)
+        index += 1
+    print(''.join(password))
+
+def main():
+    with open('in05.txt', 'rb') as f:
+        inp = f.read()
+    # day05a(inp)
+    day05b(inp)
+
+main()
